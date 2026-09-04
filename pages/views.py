a9b26572e753fca3a1a2e3963d49fd276978ed49
Angel Medina <Amedina4@buffs.wtamu.edu>
2026-09-04 17:48:38 -0500

    Add base template and named URLs

--- pages/views.py
+++ pages/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render
 
 def home_view(request):
     items = [
-        {"name": "Learn Django basics", "completed": True},
-        {"name": "Set up project repository", "completed": True},
-        {"name": "Build out personal website views", "completed": False},
-        {"name": "Pass all 14 automation tests", "completed": False},
-        {"name": "Submit repository link on Canvas", "completed": False},
+        {"name": "Learn Django basics", "completed": False},
+        {"name": "Learn more about the stock market", "completed": False},
+        {"name": "Enjoy my car as much as I can", "completed": True},
+        {"name": "Save up some money", "completed": False},
+        {"name": "Install a cam in my car", "completed": False},
     ]
     return render(request, "home.html", {"items": items})
 
